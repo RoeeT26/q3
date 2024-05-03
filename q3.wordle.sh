562599412c9e9c2ab544b6a5f659ba7ad2f2ba0c
Roee Tsahi <roeit26@gmail.com>
2024-05-03 14:39:56 +0300

    making sure the user inputs 5 alphabetical characters

--- q3.wordle.sh
+++ q3.wordle.sh
@@ -2,3 +2,7 @@
 #Q3
 
 read -p "please write a word with 5 alphabetically characters: " user_word
+
+# making sure the input word indeed has 5 alphabetical characters
+if [[ "${#user_word}" -ne 5 ]] || [[ ! "$user_word" =~ ^[a-zA-Z]{5}$ ]]; then
+echo "didn't follow instructions..."
