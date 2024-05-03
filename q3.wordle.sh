63e71119e21f42ecc65addaa59d9df08ccbae3cc
Roee Tsahi <roeit26@gmail.com>
2024-05-03 14:46:23 +0300

    adding the option to quit the game if  the user didn't follow the instructions of 5 alphabetical characters

--- q3.wordle.sh
+++ q3.wordle.sh
@@ -6,3 +6,6 @@ read -p "please write a word with 5 alphabetically characters: " user_word
 # making sure the input word indeed has 5 alphabetical characters
 if [[ "${#user_word}" -ne 5 ]] || [[ ! "$user_word" =~ ^[a-zA-Z]{5}$ ]]; then
 echo "didn't follow instructions..."
+read -p "Press enter to quit the game"
+exit 1
+fi
